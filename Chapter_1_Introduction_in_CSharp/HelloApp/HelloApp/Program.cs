1f6547ab2e74c7ea7e85ff118e3853fd814b1eda
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-04 18:20:58 -0500

    Chapter_1_Introduction_in_CSharp was added

---
+++ Chapter_1_Introduction_in_CSharp/HelloApp/HelloApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Set your name: ");
+            Console.WriteLine($"Hey {Console.ReadLine()}");
+        }
+    }
+}
