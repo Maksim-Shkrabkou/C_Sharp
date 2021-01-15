9636464ddc0c742a5806eae579b85d4c312a0cb1
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-14 22:37:02 -0500

    Add Chapter_2_CSharp_programming_basics part_2

---
+++ Chapter_2_CSharp_programming_basics/Program_Structure/Arithmetic_operations/ArithmeticOperations/ArithmeticOperations/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ArithmeticOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +
+            var x = 10;
+            var z = x + 12; // 22
+            
+            // -
+            var x1 = 10;
+            var z1 = x1 - 6; // 4
+            
+            // *
+            var x2 = 10;
+            var z2 = x2 * 5; // 50
+            
+            // /
+            var x4 = 10;
+            var z4 = x / 5; // 2
+ 
+            double a = 10;
+            double b = 3;
+            var c = a / b; // 3.33333333
+            
+            double z5 = 10 /  4; // 2
+            double z6 = 10.0 /  4.0; // 2.5
+            
+            // %
+            var x7 = 10.0;
+            double z7 = x % 4.0; // 2
+
+            // ++
+            
+            /*
+             * The increment can be prefixed: ++ x - first, the value of the variable x is increased by 1, and then its value is returned as the result of the operation.
+             * And there is also a postfix increment: x ++ - first the value of the variable x is returned as the result of the operation, and then 1 is added to it.
+            */
+            
+            var x8 = 5;
+            var z8 = ++x8; // z8=6; x8=6
+            Console.WriteLine($"{x8} - {z8}");
+ 
+            var x9 = 5;
+            var z9 = x9++; // z9=5; x9=6
+            Console.WriteLine($"{x9} - {z9}");
+            
+            // --
+            
+            var x10 = 5;
+            var z10 = --x10; // z10=4; x10=4
+            Console.WriteLine($"{x10} - {z10}");
+ 
+            var x11 = 5;
+            var z11= x11--; // z11=5; x11=4
+            Console.WriteLine($"{x11} - {z11}");
+            
+            //
+            
+            /*
+             * When performing several arithmetic operations at once, consider the order in which they are performed. Priority of operations from highest to lowest:
+             * 
+             * 1 -> Increment, decrement
+             * 2 -> Multiplication, division, getting the remainder
+             * 3 -> Addition, subtraction
+             *
+             * Brackets are used to change the order of operations.
+             */
+            
+            var a1 = 3;
+            var b1 = 5;
+            var c1 = 40;
+            var d1 = c1---b1*a1;    // a1=3  b1=5  c1=39  d1=25
+            Console.WriteLine($"a={a1}  b={b1}  c={c1}  d={d1}");
+            
+            
+            var a2 = 3;
+            var b2 = 5;
+            var c2 = 40;
+            var d2 = (c2-(--b2))*a2;    // a2=3  b2=4  c2=40  d2=108
+            Console.WriteLine($"a={a2}  b={b2}  c={c2}  d={d2}");
+            
+            // Operator associativity
+            
+            var x12 = 10 / 5 * 2; // 4
+            
+            /*
+             * When operations have the same priority, the order of evaluation is determined by the associativity of the operators. There are two types of operators depending on associativity:
+             *
+             * Left associative operators that run from left to right
+             * Right associative operators that execute from right to left
+             *
+             * All arithmetic operators (except for prefix increment and decrement) are left associative, that is, they are executed from left to right.
+             *
+             * Therefore, the expression 10/5 * 2 must be interpreted as (10/5) * 2, that is, the result will be 4.
+             */
+        }
+    }
+}
---
+++ Chapter_2_CSharp_programming_basics/Program_Structure/Bitwise_operations/BitwiseOperations/BitwiseOperations/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace BitwiseOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Logical operations
+            
+            
+            // & (logical multiplication)
+            
+            var x1 = 2; //010
+            var y1 = 5; //101
+            Console.WriteLine(x1 & y1); // 0
+             
+            var x2 = 4; //100
+            var y2 = 5; //101
+            Console.WriteLine(x2 & y2); // 4
+            
+            /*
+             * In the first case, we have two numbers 2 and 5. 2 in binary represents the number 010, and 5 - 101.
+             * Multiply the numbers (0 * 1, 1 * 0, 0 * 1) bit by bit and get 000 as a result.
+             *
+             * In the second case, instead of two, we have the number 4, which has 1 in the first bit,
+             * just like the number 5, so in the end we get (1 * 1, 0 * 0, 0 * 1) = 100, that is, the number 4 in decimal format.
+             */
+
+
+            
+            // | (logical addition)
+            
+            /*
+             * It looks like logical multiplication, the operation is also performed on binary digits,
+             * but now one is returned if at least one number in this bit has a one. For example:
+            */
+            
+            var x3 = 2; //010
+            var y3 = 5; //101
+            Console.WriteLine(x3 | y3); // 7 - 111
+            
+            var x4 = 4; //100
+            var y4 = 5; //101
+            Console.WriteLine(x4 | y4); // 5 - 101
+            
+            
+            // ^ (logical exclusive OR)
+            
+            /*
+             * Here, again, bitwise operations are performed. If we have different values of the current bit for both numbers,
+             * then 1 is returned, otherwise 0 is returned.
+             *
+             * 
+             */
+            
+            var x = 45; // The value to be encrypted is in binary form -> 101101
+            var key = 102; // Let this be the key - in binary 1100110
+            var encrypt = x ^ key; //  75
+            Console.WriteLine("Encrypted digit: " + encrypt);
+ 
+            var decrypt = encrypt ^ key; // The result will be the original number 45
+            Console.WriteLine("Decrypted digit: " + decrypt);
+            
+            
+            // ~ (logical negation or inverse)
+            
+            /*
+             * Another bitwise operation that inverts all bits: if the bit value is 1, then it becomes zero, and vice versa.
+            */
+            
+            var x5 = 12;            // 00001100
+            Console.WriteLine(~x5);  // 11110011   или -13
+            
+            
+            
+            // Representing negative numbers
+            
+            /*
+             * To write signed numbers in C #, a two's complement is used, in which the most significant bit is signed.
+             *
+             * If its value is 0, then the number is positive, and its binary representation does not differ from that of an unsigned number. For example, 0000 0001 in decimal 1.
+             * If the most significant bit is 1, then we are dealing with a negative number. For example, 1111 1111 in decimal represents -1. Accordingly, 1111 0011 represents -13.
+             *
+             * To get negative from a positive number, you need to invert it and add one: 
+            */
+
+            var x6 = 12;
+            var y6 = ~x6;
+            y6 += 1;
+            Console.WriteLine(y6); // -12
+            
+            
+            // Bitwise shifts
+            
+            /*
+             * The shift operators << and >> shift the bits in a variable left or right by a specified number.
+             * In this case, zeros are set on the vacated positions (except for a right shift of a negative number, in this case ones are set on free positions,
+             * since numbers are represented in a two's complement code and the sign bit must be maintained).
+             * Left shift can be used to multiply a number by two, right shift can be used to divide.
+             *
+             * x = 7 // 00000111 (7)
+             * x = x >> 1 // 00000011 (3)
+             * x = x << 1 // 00000110 (6)
+             * x = x << 5 // 11000000 (-64)
+             * x = x >> 2 // 11110000 (-16)
+             */
+        }
+    }
+}
