ad744557c2399609a2214168c6a02f4afb241252
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-24 18:06:22 -0500

    Add Chapter_2_CSharp_Programming_basics final part

---
+++ Chapter_2_CSharp_programming_basics/Program_Structure/Enum/Enum/Enum/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationThree op3Add = OperationThree.Add;
+            Console.WriteLine(op3Add); // Add
+            Console.WriteLine((int) op3Add); // 1
+            Console.WriteLine('\n');
+            
+            //
+            
+            var op3Subtract = OperationThree.Subtract;
+            Console.WriteLine(op3Subtract); // Subtract
+            Console.WriteLine((int) op3Subtract); // 2
+            Console.WriteLine('\n');
+            
+            //
+            
+            Operation op;
+            op = Operation.Add;
+            Console.WriteLine(op); // Add
+            Console.WriteLine((int) op); // 1
+            Console.WriteLine('\n');
+            
+            // In a program, we can assign a value to this variable.
+            // In this case, one of the constants defined in this enumeration must act as its value.
+            // That is, despite the fact that each constant is mapped to a certain number,
+            // we cannot assign a numeric value to it, for example, Operation op = 1 ;.
+            // And also if we print the value of this variable to the console, we will get constants for them,
+            // not a numeric value. If you need to get a numeric value, then you should cast to a numeric type:
+            
+            Operation op2;
+            op2 = Operation.Multiply;
+            Console.WriteLine((int) op2); // 3
+            Console.WriteLine('\n');
+
+            //
+
+            var op4 = OperationFour.Multiply;
+            Console.WriteLine(op4); // Multiply
+            Console.WriteLine('\n');
+              
+            //
+                
+            // The operation type is set using the Operation.Add constant, which is equal to 1
+            MathOp (10, 5, Operation.Add);
+            // The operation type is set using the Operation.Multiply constant, which is equal to 3
+            MathOp (11, 5, Operation.Multiply);
+            
+            // Here we have an Operation enum, which represents arithmetic operations.
+            // We also have a MathOp method defined, which takes two numbers and an operation type as parameters.
+            // In the main Main method, we call the MathOp procedure twice,
+            // passing in two numbers and the type of operation.
+        }
+
+        // Besides primitive data types, C # has such a type as enum or enumeration.
+        // Enumerations represent a collection of logically related constants.
+        // An enumeration is declared using the enum operator.
+        // Next comes the name of the enumeration, after which the type of the enumeration is indicated
+        // - it must necessarily represent an integer type (byte, int, short, long).
+        // If the type is not explicitly specified, the default is int.
+        // Then comes a comma-separated list of enumeration elements:
+        
+        enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        
+        enum Time : byte
+        {
+            Morning,
+            Afternoon,
+            Evening,
+            Night
+        }
+        
+        // In these examples, each element of the enumeration is assigned an integer value,
+        // with the first element being 0, the second 1, and so on.
+        // We can also specify the values of the elements explicitly,
+        // either by specifying the value of the first element:
+        
+        enum Operation
+        { 
+            Add = 1,   // each next element is incremented by one by default
+            Subtract,  // this element is 2
+            Multiply,  // equals 3
+            Divide     // equals 4
+        }
+        
+        // But you can also explicitly specify values for all elements:
+
+        enum OperationTwo
+        {
+            Add = 2,
+            Subtract = 4,
+            Multiply = 8,
+            Divide = 16
+        }
+        
+        // In this case, the enumeration constants can have the same values,
+        // or you can even assign one constant the value of another constant:
+        
+        enum Color
+        {
+            White = 1,
+            Black = 2,
+            Green = 2,
+            Blue = White // Blue = 1
+        }
+        
+        // Each enum actually defines a new data type.
+        // Then in the program, we can define a variable of this type and use it:
+        
+        enum OperationThree
+        {
+            Add = 1,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        
+        // Often, an enumeration variable acts as a state store, depending on which some actions are performed.
+        // So, let's consider the use of enumeration using a more real-life example:
+        
+        static void MathOp(double x, double y, Operation op)
+        {
+            var result = op switch
+            {
+                Operation.Add => x + y,
+                Operation.Subtract => x - y,
+                Operation.Multiply => x * y,
+                Operation.Divide => x / y,
+                _ => 0.0
+            };
+
+            Console.WriteLine("The result of the operation is {0}", result);
+        }
+    }
+    
+    // It is also worth noting that the enumeration does not have to be defined inside the class,
+    // it is possible outside the class, but within the namespace:
+    
+    enum OperationFour
+    {
+        Add = 1,
+        Subtract,
+        Multiply,
+        Divide
+    }
+}
---
+++ Chapter_2_CSharp_programming_basics/Program_Structure/Tuples/Tuples/Tuples/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tuples provide a convenient way to work with a set of values that was added in C # 7.0.
+
+            // A tuple represents a set of values enclosed in parentheses:
+
+            var tuple = (5, 10);
+            
+            // In this case, a tuple is defined, which has two values: 5 and 10.
+            // In the future, we can refer to each of these values
+            // through the fields named Item [ordinal_number_of_field_in_tuple]. For example:
+            
+            Console.WriteLine(tuple.Item1); // 5
+            Console.WriteLine(tuple.Item2); // 10
+            tuple.Item1 += 26;
+            Console.WriteLine(tuple.Item1); // 31
+            Console.WriteLine('\n');
+            
+            // In this case, the type is determined implicitly.
+            // But we can also explicitly specify the type for a tuple variable:
+
+            (int, int) tuple2 = (5, 10);
+            
+            // Since a tuple contains two numbers, we need to specify two numeric types in the type definition.
+            // Or another example of defining a tuple:
+
+            (string, int, double) tuple3 = ("Hi", 5, 88.88);
+            
+            // The first element of the tuple in this case represents a string,
+            // the second element is of type int, and the third is of type double.
+
+            // We can also name the fields of the tuple:
+            
+            var tuple4 = (count:5, sum:10);
+            Console.WriteLine(tuple4.count); // 5
+            Console.WriteLine(tuple4.sum); // 10
+            Console.WriteLine('\n');
+            
+            // Now, to refer to the fields of the tuple, their names are used, rather than the names Item1 and Item2.
+            
+            Console.WriteLine(tuple4.count);
+            Console.WriteLine(tuple4.sum);
+            Console.WriteLine('\n');
+
+            // We can even not use a variable to define the entire tuple, but use separate variables for its fields:
+            
+            var (name, age) = ("Tom", 23);
+            Console.WriteLine(name);    // Tom
+            Console.WriteLine(age);     // 23
+            Console.WriteLine('\n');
+            
+            // In this case,
+            // we can work with the fields of the tuple as with the variables that are defined within the method.
+            
+            
+            // 
+            
+            // Using tuples
+            
+            // Tuples can be passed as parameters to a method,
+            // they can be the return result of a function, or they can be used in some other way.
+
+            // For example, one of the common situations is when a function returns two or more values,
+            // while a function can only return one value. And tuples represent the optimal way to accomplish this task:
+            
+            var tuple5 = GetValues();
+            Console.WriteLine(tuple5.Item1); // 1
+            Console.WriteLine(tuple5.Item2); // 3
+            Console.WriteLine('\n');
+            
+            // Here's a GetValues() method that returns a tuple.
+            
+            // A tuple is defined as a set of values, enclosed in parentheses.
+            // And in this case, we return a tuple of two elements of type int, that is, two numbers.
+
+            // Another example:
+            
+            var tuple6 = GetNamedValues(new int[]{ 1,2,3,4,5,6,7});
+            Console.WriteLine(tuple6.count);
+            Console.WriteLine(tuple6.sum);
+            Console.WriteLine('\n');
+            
+            // And also a tuple can be passed as a parameter to a method:
+            
+            var (name1, age1) = GetTuple(("Tom", 23), 12);
+            Console.WriteLine(name1);    // Tom
+            Console.WriteLine(age1);     // 35
+            
+        }
+        
+        
+        private static (int, int) GetValues()
+        {
+            var result = (1, 3);
+            
+            return result;
+        }
+        
+        private static (int sum, int count) GetNamedValues(int[] numbers)
+        {
+            var result = (sum:0, count: 0);
+            
+            for (var i=0; i < numbers.Length; i++)
+            {
+                result.sum += numbers[i];
+                result.count++;
+            }
+            return result;
+        }
+        
+        // And also a tuple can be passed as a parameter to a method:
+        private static (string name, int age) GetTuple((string n, int a) tuple, int x)
+        {
+            var result = (name: tuple.n, age: tuple.a + x);
+            
+            return result;
+        }
+    }
+}
