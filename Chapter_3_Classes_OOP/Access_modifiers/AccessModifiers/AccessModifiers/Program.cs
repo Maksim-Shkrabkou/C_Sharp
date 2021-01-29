d805f9474da5c3972d5b51a242ccc517f4353a27
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-29 00:13:11 -0500

    Add Chapter_3_Classes_OOP part_3

---
+++ Chapter_3_Classes_OOP/Access_modifiers/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var state1 = new State();
+ 
+            // we won't be able to assign the value to the variable defaultVar,
+            // since it has a private modifier and the Program class does not see it
+            // And the environment will underline this line as incorrect
+            //state1.defaultVar = 5; // Error, cannot be accessed
+ 
+            // the same applies to the privateVar variable
+            //state1.privateVar = 5; // Error, cannot be accessed
+ 
+            // assigning the value to the protectedPrivateVar variable will fail,
+            // since the Program class is not a subclass of the State class
+            //state1.protectedPrivateVar =5; // Error, cannot be accessed
+ 
+            // assigning a value to the protectedVar variable will also fail,
+            // since the Program class is not a subclass of the State class
+            //state1.protectedVar = 5; // Error, cannot be accessed
+            
+            
+            // the internalVar variable with the internal modifier is accessible from anywhere in the current project
+            // so safely assign a value to it
+            state1.internalVar = 5;
+ 
+            // the protectedInternalVar variable is also accessible from anywhere in the current project
+            state1.protectedInternalVar = 5;
+ 
+            // the publicVar variable is public
+            state1.publicVar = 5;
+            
+            
+            //state1.defaultMethod(); // Error, cannot be accessed
+            
+            // state1.privateMethod(); // Error, cannot be accessed
+ 
+            // state1.protectedPrivateMethod(); // Error, cannot be accessed
+ 
+            // state1.protectedMethod(); // Error, cannot be accessed
+ 
+            state1.internalMethod();    // work
+ 
+            state1.protectedInternalMethod();  // work
+ 
+            state1.publicMethod();      // work
+        }
+    }
+    
+    // All class members - fields, methods, properties - they all have access modifiers.
+    // Access modifiers allow you to set a set scope for class members.
+    //
+    // That is, access modifiers define the context in which a service variable or method can be used.
+    // In the previous topics, we have already encountered
+    // it when we declared it public (that is, with the public modifier).
+    
+    
+    // The following access modifiers are used in C #:
+
+    // public: public, public class, or class member.
+    // Such a class member is accessible from anywhere in the code, as well as from other programs and assemblies.
+
+    // private: private class or class member.
+    // Represents the exact opposite of the public modifier.
+    // Such a private class or class member is only accessible from code in the same class or context.
+
+    // protected: Such a class member is accessible from anywhere in the current class or in derived classes.
+    // However, derived classes can be located in other assemblies.
+
+    // internal: a class and members of a class with a similar modifier are accessible
+    // from anywhere in the code in the same assembly,
+    // but it is not available to other programs and assemblies (as is the case with the public modifier).
+    
+    // protected internal: combines the functionality of two modifiers.
+    // Classes and class members with this modifier are available from the current assembly and from derived classes.
+
+    // private protected: Such a class member is accessible from anywhere in the current class
+    // or in derived classes that are defined in the same assembly.
+    
+    
+    // We can explicitly set the access modifier, for example:
+    
+    /*
+     * private protected class State
+     * {
+     *      internal int a;
+     *      protected void Print()
+     *      { 
+                Console.WriteLine($"a = {a}"); 
+            }
+        }
+     */
+    
+    // Or we can not specify:
+    
+    /*
+     * class State
+     * {
+            int a;
+            void Print() 
+            { 
+                Console.WriteLine($"a = {a}"); 
+            }
+        }
+     */
+    
+    // If no access modifier is defined for fields and methods, the !!!'private!!!' modifier is used by default.
+
+    // Classes and structures declared without a modifier have !!!'internal'!!! access by default.
+
+    // All classes and structures that are directly defined in namespaces and
+    // that are not nested within other classes can only have public or internal modifiers.
+
+    // Let's look at an example and create the following State class:
+    
+    public class State
+    {
+        
+        // is the same as private int defaultVar;
+        int defaultVar;
+        // the field is only accessible from the current class
+        private int privateVar;
+        // accessible from the current class and derived classes that are defined in the same project
+        protected private int protectedPrivateVar;
+        // accessible from the current class and derived classes
+        protected int protectedVar;
+        // available anywhere in the current project
+        internal int internalVar;
+        // available anywhere in the current project and from derived classes in other projects
+        protected internal int protectedInternalVar;
+        // available anywhere in the program, as well as for other programs and assemblies
+        public int publicVar;
+ 
+        // has a private modifier by default
+        void defaultMethod() => Console.WriteLine($"defaultVar = {defaultVar}");
+ 
+        // the method is only accessible from the current class
+        private void privateMethod() => Console.WriteLine($"privateVar = {privateVar}");
+ 
+        // accessible from the current class and derived classes that are defined in the same project
+        protected private void protectedPrivateMethod() => Console.WriteLine($"protectedPrivateVar = {protectedPrivateVar}");
+ 
+        // accessible from the current class and derived classes
+        protected void protectedMethod()=> Console.WriteLine($"protectedVar = {protectedVar}");
+     
+        // available anywhere in the current project
+        internal void internalMethod() => Console.WriteLine($"internalVar = {internalVar}");
+     
+        // available anywhere in the current project and from inherited classes in other projects
+        protected internal void protectedInternalMethod() => Console.WriteLine($"protectedInternalVar = {protectedInternalVar}");
+     
+        // available anywhere in the program, as well as for other programs and assemblies
+        public void publicMethod() => Console.WriteLine($"publicVar = {publicVar}");
+    }
+    
+    // Since the State class is declared with the public modifier,
+    // it will be accessible from anywhere in the program, as well as from other programs and assemblies.
+    // The State class has five fields for each access level.
+    // Plus one variable without a modifier, which is private by default.
+
+    // There are also six methods that will display the values of the class fields on the screen.
+    // Please note that since all modifiers allow you to use class members inside this class,
+    // then all class variables, including private ones, are available to all of its methods,
+    // since all are in the context of the State class.
+    
+    // Now let's see how we can use the variables of our class in the program
+    // (that is, in the Main method of the Program class) if the State and Program classes are in the same project:
+    
+    // Thus, we were able to set only the variables internalVar, protectedInternalVar and publicVar,
+    // since their modifiers allow us to use them in this context.
+
+    // The situation is similar with the methods:
+    
+    // Here, only three methods were available to us:
+    // internalMethod, protectedInternalMethod, publicMethod, which have the internal, protected internal,
+    // public modifiers, respectively.
+
+    // Thanks to this system of access modifiers,
+    // it is possible to hide some aspects of the class implementation from other parts of the program.
+
+    // Despite the fact that the public and internal modifiers are similar in their action,
+    // they have a big difference. Classes and members of a class with the public modifie
+    // r will also be available to other programs
+    // if the given class is placed in a dynamic library dll and then used in these programs.
+}
---
+++ Chapter_3_Classes_OOP/Properties/Properties/Properties/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // 1!
+            var p = new Person();
+ 
+            // Set the property - the Set block is triggered
+            // the value "Tom" is passed to the value property
+            p.Name = "Tom";
+    
+            // Get the property value and assign it to a variable - the Get block is triggered
+            var personName = p.Name; 
+            
+            // 2!
+            
+            var p5 = new Person5("Tom");
+ 
+            // Error - set is declared with the private modifier
+            // p5.Name = "John";
+ 
+            Console.WriteLine(p5.Name);
+            Console.WriteLine('\n');
+            
+            // 3!
+            
+            var person = new Person7();
+            Console.WriteLine(person.Name); // Tom
+            Console.WriteLine(person.Age);  // 23
+
+        } 
+
+        // In addition to regular methods, C # provides special accessor methods called properties.
+        // They provide easy access to the fields of classes and structures, find out their meaning or set them.
+
+        // The standard property description has the following syntax:
+        
+        // [access_modifier] return_type arbitrary_name
+        // {
+            // property code
+        // }
+        
+        // For example:
+        
+        class Person
+        {
+            private string _name;
+ 
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+ 
+                set
+                {
+                    _name = value;
+                }
+            }
+        }
+        
+        // Here we have a private name field and a public property Name.
+        // Although they have almost the same name except for the case,
+        // but this is nothing more than a style, their names can be arbitrary and do not have to be the same.
+
+        // Through this property, we can control access to the name variable.
+        // The standard property definition contains get and set blocks.
+        // In the get block we return the value of the field,
+        // and in the set block we set. The value parameter represents the value to pass.
+
+        // We can use this property as follows:
+        
+        // 1!
+        
+        // Perhaps the question may arise, why do we need properties,
+        // if in this situation we can get by with ordinary class fields?
+        //
+        // But properties allow you to nest additional logic that may be necessary
+        // , for example, when assigning a value to a class variable.
+        //
+        // For example, we need to set a check by age:
+        
+        class Person2
+        {
+            private int _age;
+ 
+            public int Age
+            {
+                set
+                {
+                    if (value < 18)
+                    {
+                        Console.WriteLine("Возраст должен быть больше 17");
+                    }
+                    else
+                    {
+                        _age = value;
+                    }
+                }
+                
+                get { return _age; }
+            }
+        }
+        
+        
+        // If the variable age were public, then we could pass it from outside any value, including negative.
+        // The property allows you to hide these objects and mediate access to them.
+
+        // The set and get blocks do not have to be present in the property at the same time.
+        // If a property is defined only by a get block, then such a property is read-only - we can get its value,
+        // but not set it. Conversely, if a property only has a set block,
+        // then that property is write-only - you can only set a value, but cannot get:
+        
+        class Person3
+        {
+            private string _name;
+            
+            // read-only property
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+            }
+ 
+            private int age;
+            
+            // write-only property
+            public int Age
+            {
+                set
+                {
+                    age = value;
+                }
+            }
+        }
+        
+        // Although in the examples above, properties were defined in a class,
+        // but in the same way we can define and use properties in structures.
+        
+        
+        //
+        
+        
+        // !!!Access modifiers!!!
+        
+        // We can apply access modifiers not only to the entire property,
+        // but also to individual blocks - either get or set:
+        
+        class Person5
+        {
+            private string _name;
+ 
+            public string Name
+            {
+                get
+                {
+                    return _name;
+                }
+ 
+                private set
+                {
+                    _name = value;
+                }
+            }
+            
+            public Person5(string name)
+            {
+                Name = name;
+            }
+        }
+        
+        // Now we can use the private set block only in this class
+        // - in its methods, properties, constructor, but not in another class:
+        
+        // 2!
+
+        
+        // When using modifiers in properties, you should take into account a number of restrictions:
+
+        // A modifier for a set or get block can be set if the property has both blocks (set and get)
+
+        // Only one set or get block can have an access modifier, but not both.
+
+        // The access modifier of a set or get block must be more restrictive than the access modifier of a property.
+        // For example, if a property has a public modifier,
+        // then a set / get block can only have the modifiers protected internal, internal, protected, private.
+        
+        
+        //
+        
+        // !!!Automatic properties!!!
+        // Properties control access to the fields of the class.
+        // However, what if we have a dozen or more fields,
+        // then it would be tedious to define each field and write a property of the same type for it.
+        // Therefore, automatic properties have been added to the .NET framework. They have an abbreviated declaration:
+        
+        class Person6
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+         
+            public Person6(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+        
+        // In fact, fields for properties are also created here,
+        // only they are not created by the programmer in the code,
+        // but the compiler automatically generates them during compilation.
+
+        // What is the advantage of autoproperties,
+        // if in essence they just refer to an automatically created variable,
+        // why not directly access a variable without autoproperties? The fact is that at any moment in time,
+        // if necessary, we can expand the auto-property into a regular property, add some specific logic to it.
+
+        // Keep in mind that you cannot create an automatic write-only property, as is the case with standard properties.
+
+        // Autoproperties can be assigned default values (initializing autoproperties):
+        
+        class Person7
+        {
+            public string Name { get; set; } = "Tom";
+            public int Age { get; set; } = 23;
+        }
+        
+        // 3!
+        
+        // And if we do not specify the values of the Name and Age properties for the Person object,
+        // then the default values will apply.
+
+        // It is worth noting that we cannot use auto-property initialization in structures.
+
+        // Autoproperties can also have access modifiers:
+        
+        class Person8
+        {
+            public string Name { private set; get;}
+            
+            public Person8(string n)
+            {
+                Name = n;
+            }
+        }
+        
+        // We can remove the set block and make the auto property read-only.
+        // In this case, to store the value of this property,
+        // a field with the readonly modifier will be implicitly created for it,
+        // so it should be borne in mind that such get-properties can be set either from the class constructor,
+        // as in the example above, or when initializing the property:
+        
+        class Person9
+        {
+            public string Name { get; } = "Tom";
+        }
+        
+        
+        //
+        
+        
+        // !!!Abbreviated notation of properties!!!
+        // Just like methods, we can shorten properties. For example:
+        
+        class Person10
+        {
+            private string _name;
+     
+            // equivalent to public string Name { get { return _name; } }
+            public string Name => _name;
+        }
+    }
+}
