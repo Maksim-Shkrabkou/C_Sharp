2add6dec439770ab3cee6ef95e0f00270e628fcc
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-29 21:18:05 -0500

    Add Chapter_3_Classes_OOP part_4

---
+++ Chapter_3_Classes_OOP/Method_overloading/MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            var calc = new Calculator();
+            calc.Add(1, 2); // 3
+            calc.Add(1, 2, 3); // 6
+            calc.Add(1, 2, 3, 4); // 10
+            calc.Add(1.4, 2.5); // 3.9
+        }
+        
+        // Sometimes it becomes necessary to create the same method, but with a different set of parameters.
+        // And depending on the available parameters, apply a specific version of the method.
+        // This feature is also called method overloading.
+
+        // And in C #, we can create several methods in a class with the same name but different signatures.
+        // What is a signature? The signature consists of the following aspects:
+
+        // Method name
+
+        // Number of parameters
+
+        // Parameter types
+
+        // Parameter order
+
+        // Parameter modifiers
+
+        // But the names of the parameters are NOT included in the signature. For example, take the following method:
+        
+        /*
+         * public int Sum(int x, int y) 
+            { 
+                return x + y;
+            }
+         */
+        
+        // For this method, the signature will look like this: Sum (int, int)
+
+        // And method overloading consists in the fact that methods have different signatures,
+        // in which only the name of the method matches. That is, the methods should differ in:
+
+        // Number of parameters
+
+        // Parameter type
+
+        // Order of parameters
+
+        // Parameter modifiers
+
+        // For example, let's say we have the following class:
+        
+        class Calculator
+        {
+            public void Add(int a, int b)
+            {
+                var result = a + b;
+                Console.WriteLine($"Result is {result}");
+            }
+            
+            public void Add(int a, int b, int c)
+            {
+                var result = a + b + c;
+                Console.WriteLine($"Result is {result}");
+            }
+            
+            public int Add(int a, int b, int c, int d)
+            {
+                var result = a + b + c + d;
+                Console.WriteLine($"Result is {result}");
+                return result;
+            }
+            
+            public void Add(double a, double b)
+            {
+                double result = a + b;
+                Console.WriteLine($"Result is {result}");
+            }
+        }
+        
+        // There are four different versions of the Add method, that is, four overloads of this method are defined.
+
+        // The first three versions of the method differ in the number of parameters.
+        // The fourth version coincides with the first in the number of parameters,
+        // but differs in their type. Moreover, it is sufficient that at least one parameter differs in type.
+        // Therefore, this is also a valid overload of the Add method.
+
+        // That is, we can represent the signatures of these methods as follows:
+        
+        // Add(int, int)
+        // Add(int, int, int)
+        // Add(int, int, int, int)
+        // Add(double, double)
+        
+        // After identifying the overloaded versions, we can use them in the program:
+        
+        // 1!
+        
+        // Also, overloaded methods may differ in the modifiers used. For example:
+        
+        /*
+         * void Increment(ref int val)
+           {
+                val++;
+                Console.WriteLine(val);
+            }
+ 
+            void Increment(int val)
+            {
+                val++;
+                Console.WriteLine(val);
+            }
+         */
+        
+        
+        // In this case, both versions of the Increment method have the same set of parameters of the same type,
+        // but in the first case, the parameter has a ref modifier.
+        // Therefore, both versions of the method will be valid overloads of the Increment method.
+        
+
+        // And the difference between methods in return type or in parameter names is not a reason for overloading.
+        // For example, take the following set of methods:
+        
+        /*
+         *  int Sum(int x, int y)
+            {
+                return x + y;
+            }
+
+            int Sum(int number1, int number2)
+            {
+                return x + y;
+            }
+
+            void Sum(int x, int y)
+            {
+                Console.WriteLine(x + y);
+            }
+         */
+        
+        // All these methods will have the same signature:
+        
+        // Sum (int, int)
+        
+        // Therefore, this set of methods does not represent correct overloads of the Sum method and will not work.
+    }
+}
---
+++ Chapter_3_Classes_OOP/Static_members_and_static_modifier/StaticMembersAndStaticModifier/StaticMembersAndStaticModifier/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace StaticMembersAndStaticModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // 1!
+            Console.WriteLine(Account.bonus);      // 100
+            Account.bonus += 200;
+         
+            var account1 = new Account(150);
+            Console.WriteLine(account1.totalSum);   // 450
+ 
+ 
+            var account2 = new Account(1000);
+            Console.WriteLine(account2.totalSum);   // 1300
+            Console.WriteLine('\n');
+            
+            // 2!
+            var user1 = new User();
+            var user2 = new User();
+            var user3 = new User();
+            var user4 = new User();
+            var user5 = new User();
+
+            User.DisplayCounter(); // 5
+            Console.WriteLine('\n');
+            
+            // 3!
+            var user21 = new User2(); // here the static constructor will fire
+            var user22 = new User2();
+        }
+        
+        // In addition to ordinary fields, methods, properties, a class can have static fields, methods, properties.
+        // Static fields, methods, properties refer to the entire class,
+        // and it is not necessary to create an instance of the class to refer to such class members. For example:
+        
+        // 1!
+        
+        class Account
+        {
+            public static decimal bonus = 100;
+            public decimal totalSum;
+            
+            public Account(decimal sum)
+            {
+                totalSum = sum + bonus; 
+            }
+        }
+        
+        // In this case, the Account class has two fields: bonus and totalSum.
+        // The bonus field is static, so it stores the state of the class as a whole,
+        // not a separate object. And so we can refer to this field by the class name:
+        
+        /*
+         * Console.WriteLine(Account.bonus);
+         * Account.bonus += 200;
+         */
+        
+        // At the memory level, a section in memory will be created for static fields,
+        // which will be common to all objects of the class.
+        
+        // !!!In this case, memory for static variables is allocated even if no object of this class has been created!!!.
+
+        
+        //
+        
+        
+        // !!!Static properties and methods!!!
+        
+        // Similarly, we can create and use static methods and properties:
+        
+        class Account2
+        {
+            public Account2(decimal sum, decimal rate)
+            {
+                if (sum < MinSum) throw new Exception("Invalid amount!");
+                Sum = sum; Rate = rate;
+            }
+            
+            private static decimal minSum = 100; // minimum allowable amount for all accounts
+            public static decimal MinSum
+            {
+                get { return minSum; }
+                set { if(value>0) minSum = value; }
+            }
+ 
+            public decimal Sum { get; private set; }    // account amount
+            public decimal Rate { get; private set; }   // interest rate
+ 
+            // calculating the amount on the account after a certain period at a certain rate
+            public static decimal GetSum(decimal sum, decimal rate, int period)
+            {
+                decimal result = sum;
+                
+                for (int i = 1; i <= period; i++)
+                    result = result + result * rate / 100;
+                
+                return result;
+            }
+        }
+        
+        // The minSum variable, the MinSum property,
+        // and the GetSum method are defined here with the static keyword, that is, they are static.
+
+        // The minSum variable and the MinSum property represent the minimum amount that is allowed to create an invoice.
+        // This indicator does not refer to any particular account, but refers to all accounts as a whole.
+        // If we change this figure for one account, then it must also change for the other account.
+        // That is, unlike the Sum and Rate properties, which store the state of an object,
+        // the minSum variable stores the state for all objects of this class.
+        
+        // The same is with the GetSum method - it calculates the amount
+        // in the account after a certain period at a certain interest rate for a certain initial amount.
+        // The call and result of this method is independent of a particular object or its state.
+
+        // Thus, variables and properties that store state common to all objects in a class should be defined as static.
+        // And also methods that define the behavior common to all objects should also be declared as static.
+
+        // Static members of a class are common to all objects of this class,
+        // so they must be referred to by the class name:
+        
+        /*
+         * Account.MinSum = 560;
+         * decimal result = Account.GetSum(1000, 10, 5);
+         */
+        
+        // !!!Keep in mind that static methods can only access static members of a class.
+        // We cannot refer to non-static methods, fields, properties inside a static method.!!!
+
+        // Static fields are often used to store counters. F
+        // or example, let's say we have a User class,
+        // and we want to have a counter that lets us know how many User objects have been created:
+        
+        // 2!
+        
+        class User
+        {
+            private static int counter = 0;
+            
+            public User()
+            {
+                counter++;
+            }
+ 
+            public static void DisplayCounter()
+            {
+                Console.WriteLine($"Created {counter} objects User");
+            }
+        }
+        
+        
+        //
+        
+        
+        // !!!Static constructor!!!
+        
+        // In addition to regular constructors,
+        // a class can also have static constructors. Static constructors have the following distinctive features:
+
+        // Static constructors must not have an accessor modifier and take no parameters
+
+        // As with static methods, static constructors cannot use the this keyword to refer to the current class object
+        // and can only access static members of the class.
+
+        // Static constructors cannot be called manually in a program.
+        // They are executed automatically at the very first creation of an object of this class
+        // or at the first call to its static members (if any)
+        
+        // Static constructors are usually used to initialize static data,
+        // or perform actions that only need to be done once.
+
+        // Let's define a static constructor:
+        
+        // 3!
+        class User2
+        {
+            static User2()
+            {
+                Console.WriteLine("First user created");
+            }
+        }
+        
+        
+        //
+        
+        
+        // !!!Static classes!!!
+        
+        // Static classes are declared with the static modifier
+        // and can only contain static fields, properties, and methods.
+        
+        // For example, if the Account class had only static variables, properties and methods,
+        // then it could be declared as static:
+        
+        static class Account3
+        {
+            private static decimal minSum = 100; // minimum allowable amount for all accounts
+            
+            public static decimal MinSum
+            {
+                get { return minSum; }
+                set { if(value>0) minSum = value; }
+            }
+ 
+            // calculating the amount on the account after a certain period at a certain rate
+            public static decimal GetSum(decimal sum, decimal rate, int period)
+            {
+                decimal result = sum;
+                
+                for (int i = 1; i <= period; i++)
+                    result = result + result * rate / 100;
+                
+                return result;
+            }
+            
+            // In C #, an illustrative example of a static class is the Math class,
+            // which is used for various mathematical operations.
+        }
+    }
+}
