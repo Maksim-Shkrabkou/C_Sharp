9de00f7123714e83025243de3c7b5049140a2ba9
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-30 20:32:17 -0500

    Add Chapter_3_Classes_OOP part_5

---
+++ Chapter_3_Classes_OOP/Operator_overloading/OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            var c1 = new Counter { Value = 23 };
+            var c2 = new Counter { Value = 45 };
+ 
+            var result = c1 > c2;
+            var c3 = c1 + c2;
+            
+            // 2!
+            var c4 = new Counter { Value = 23 };
+            var c5 = new Counter { Value = 45 };
+            var result2 = c4 > c5;
+            Console.WriteLine(result2); // false
+            Console.WriteLine('\n');
+ 
+            var c6 = c4 + c5;
+            Console.WriteLine(c6.Value);  // 23 + 45 = 68
+            Console.WriteLine('\n');
+            
+            // 3!
+            
+            var c7 = new Counter { Value = 23 };
+            var d = c7 + 27; // 50
+            Console.WriteLine(d);
+            Console.WriteLine('\n');
+
+            // 4!
+            var counter = new Counter() { Value = 10 };
+            Console.WriteLine($"{counter.Value}");      // 10
+            Console.WriteLine($"{(++counter).Value}");  // 20
+            Console.WriteLine($"{counter.Value}");      // 20
+            Console.WriteLine('\n');
+            
+            // 5!
+            var counter2 = new Counter() { Value = 10 };
+            Console.WriteLine($"{counter2.Value}");      // 10
+            Console.WriteLine($"{(counter2++).Value}");  // 10
+            Console.WriteLine($"{counter2.Value}");      // 20
+            Console.WriteLine('\n');
+            
+            // 6!
+            var counter3 = new Counter() { Value = 0 };
+            if (counter3)
+                Console.WriteLine(true);
+            else
+                Console.WriteLine(false);
+        }
+        
+        // Along with methods, we can also overload operators.
+        // For example, suppose we have the following Counter class:
+        
+        /*class Counter
+         {
+            public int Value { get; set; }
+         }
+        */
+        
+        // This class represents some counter, the value of which is stored in the Value property.
+        
+        // And let's say we have two objects of the Counter class - two counters that we want to compare
+        // or add based on their Value properties using standard comparison and addition operations:
+        
+        // 1!
+        
+        /*
+         * public static return_type operator operator (parameters)
+         * {}
+         */
+        
+        // This method must have public static modifiers, as the overloaded operator
+        // will be used for all objects of this class. Next is the name of the return type.
+        // The return type represents the type from which we want to get the objects.
+        // For example, as a result of adding two Counter objects, we expect to get a new Counter object.
+        // And as a result of comparing the two, we want to get an object of type bool,
+        // which indicates whether the conditional expression is true or false.
+        // But depending on the task, the return types can be anything.
+        
+        // Then, instead of the method name, there is the operator keyword and the operator itself.
+        // And then parameters are listed in brackets.
+        // Binary operators take two parameters, unary operators take one parameter.
+        // And in any case,
+        // one of the parameters must represent the type - the class or structure in which the operator is defined.
+
+        // For example, let's overload a number of operators for the Counter class:
+        
+        class Counter
+        {
+            public int Value { get; set; }
+         
+            public static Counter operator +(Counter c1, Counter c2)
+            {
+                return new Counter { Value = c1.Value + c2.Value };
+            }
+            public static bool operator >(Counter c1, Counter c2)
+            {
+                return c1.Value > c2.Value;
+            }
+            public static bool operator <(Counter c1, Counter c2)
+            {
+                return c1.Value < c2.Value;
+            }
+            
+            public static int operator +(Counter c1, int val)
+            {
+                return c1.Value + val;
+            }
+            
+            public static Counter operator ++(Counter c1)
+            {
+                return new Counter { Value = c1.Value + 10 };
+            }
+            
+            public static bool operator true(Counter c1)
+            {
+                return c1.Value != 0;
+            }
+            public static bool operator false(Counter c1)
+            {
+                return c1.Value == 0;
+            }
+        }
+        
+        // Since all overloaded operators are binary - that is,
+        // they are performed over two objects, then for each overload there are two parameters.
+
+        // Since in the case of the addition operation we want to add two objects of the Counter class,
+        // the operator takes two objects of this class.
+        // And since we want to get a new Counter object as a result of addition,
+        // this class is also used as the return type.
+        // All actions of this operator are reduced to creating a new object,
+        // the Value property of which combines the values of the Value property of both parameters:
+        
+        /*
+         * public static Counter operator +(Counter c1, Counter c2)
+          {
+            return new Counter { Value = c1.Value + c2.Value };
+          }
+         */
+        
+        // Two comparison operators have also been redefined.
+        // If we override one of these comparison operations, then we must also override the second of these operations.
+        // The comparison operators themselves compare the values of the Value properties and,
+        // depending on the result of the comparison, return either true or false.
+
+        // Now we use overloaded operators in the program:
+        
+        // 2!
+        
+        // It is worth noting that since, in fact, the definition of an operator is a method,
+        // we can also overload this method, that is, create another version for it.
+        //
+        // For example, let's add another operator to the Counter class:
+        
+        /*
+         * public static int operator +(Counter c1, int val)
+         * {
+         *      return c1.Value + val;
+         * }
+         */
+        
+        // This method adds the value of the Value property and a number, returning their sum.
+        // And we can also use this operator:
+        
+        // 3!
+        
+        // It should be borne in mind that during overloading,
+        // those objects that are passed to the operator through parameters should not be changed.
+        // For example, we can define an increment operator for the Counter class:
+        
+        /*
+         * public static Counter operator ++(Counter c1)
+         * {
+         *      c1.Value += 10;
+         *
+         *      return c1;
+         * }
+         */
+        
+        // Since the operator is unary,
+        // it takes only one parameter - an object of the class in which this operator is defined.
+        // But this is an incorrect definition of the increment,
+        // since the operator should not change the values of its parameters.
+
+        // And a more correct overload of the increment operator would look like this:
+        
+        /*
+         * public static Counter operator ++(Counter c1)
+         * {
+                return new Counter { Value = c1.Value + 10 };
+           }
+         */
+        
+        // That is, a new object is returned that contains the incremented value in the Value property.
+
+        // At the same time, we do not need to define separate operators for
+        // prefix and postfix increments (as well as decrements), since one implementation will work in both cases.
+
+        // For example, let's use the prefix increment operation:
+        
+        // 4!
+        
+        // Now we use the postfix increment:
+        
+        // 5!
+        
+        // It is also worth noting that we can override the true and false operators.
+        // For example, let's define them in the Counter class:
+        
+        // "in Counter class"
+        
+        // These operators are overloaded when we want to use an object of the type as a condition. For example:
+        
+        // 6!
+        
+        // When overloading operators, keep in mind that not all operators can be overloaded.
+        // In particular, we can overload the following operators:
+
+        // unary operators +, -,!, ~, ++, -
+
+        // binary operators +, -, *, /,%
+
+        // comparison operations ==,! =, <,>, <=,> =
+        
+        // logical operators &&, ||
+
+        // And there are a number of operators that cannot be overloaded,
+        // such as the equality operator = or the ternary operator?:, As well as a number of others.
+
+        // A complete list of overloaded operators can be found in the msdn documentation
+    
+        // When overloading operators,
+        // you should also remember that we cannot change the precedence of an operator or its associativity,
+        // we cannot create a new operator or change the logic of operators in types, which is by default in .NET.
+    }
+}
---
+++ Chapter_3_Classes_OOP/Reading_constants_fields_and_structures/ReadingConstantsFieldsAndStructures/ReadingConstantsFieldsAndStructures/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace ReadingConstantsFieldsAndStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            //MathLib.E=3.8; // Error, constant value cannot be changed
+            
+            // 2!
+            Console.WriteLine(MathLib.PI);
+            Console.WriteLine('\n');
+            
+            // 3!
+            var mathLib3 = new MathLib3(3.8);
+            Console.WriteLine(mathLib3.K); // 3.8
+            Console.WriteLine('\n');
+            
+            //mathLib3.K = 7.6; // read field cannot be set outside of its class
+        }
+        
+        // Class fields are ordinary class-level variables.
+        // We have already looked at variables - their declaration and initialization.
+        // However, we have not yet touched on some points, for example, constants and read fields.
+
+        
+        //
+         
+        
+        // !!!Constants!!!
+        
+        // Constants are characterized by the following features:
+
+        // Constant must be initialized when defining
+
+        // Once defined, the value of the constant cannot be changed
+
+        // Constants are intended to describe values that should not be changed in the program.
+        // The const keyword is used to define constants:
+            
+        const double PI = 3.14;
+        const double E = 2.71;
+            
+        // When using constants, remember that we can declare them only once
+        // and that they must be defined by the time of compilation.
+        
+        class MathLib
+        {
+            public const double PI=3.141;
+            public const double E = 2.81;
+            // public const double K;      // Error, constant not initialized
+        }
+        
+        // 1!
+        
+        
+        // Also note the syntax for calling a constant.
+        // Since this is implicitly a static field, you must use the class name to refer to it.
+        
+        // But it should be borne in mind that we cannot declare a constant with the static modifier.
+        // But this actually makes no sense.
+
+        // A constant can be defined both at the class level and inside a method:
+        
+        class MathLib2
+        {
+            public double GetCircleArea(double radius)
+            {
+                const double PI = 3.141;
+                
+                return PI * radius * radius;
+            }
+        }
+        
+        
+        //
+        
+        
+        // !!!Reading fields!!!
+        
+        // Read fields can be initialized when declared, either at the class level,
+        // or initialized and modified in the constructor.
+        // You cannot initialize or change their value elsewhere, you can only read their value.
+
+        // The read field is declared with the readonly keyword:
+        
+        class MathLib3
+        {
+            public readonly double K = 23;  // can be initialized like this
+ 
+            public MathLib3(double _k)
+            {
+                K = _k; // the read field can be initialized or changed in the constructor after compilation
+            }
+            
+            public void ChangeField()
+            {
+                // you can not do it this way
+                // K = 34;
+            }
+        }
+        
+        // !!!Comparison of constants!!!
+            
+        // Constants must be defined at compile time, and readable fields can be defined at run time.
+        // Accordingly, a constant can be initialized only when it is defined.
+        // The read field can be initialized either when it is defined or in the class constructor.
+        
+        // Constants cannot use the static modifier, since they are already implicitly static.
+        // Reading fields can be static or non-static.
+        
+        
+        //
+        
+        
+        // !!!Read structures!!!
+        
+        // In addition to readable fields, C # can define readable structures.
+        // To do this, they are preceded by the readonly modifier:
+        
+        // readonly struct User { }
+        
+        // A feature of such structures is that all their fields must also be readable:
+        
+        readonly struct User
+        {
+            public readonly string Name;
+            
+            public User(string name)
+            {
+                this.Name = name;
+            }
+        }
+        
+        // The same goes for properties that should be read-only:
+        
+        readonly struct User2
+        {
+            public readonly string Name { get; } //read-only is optional
+            public int Age { get; } // read-only property
+
+            public User2(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+        }
+    }
+}
