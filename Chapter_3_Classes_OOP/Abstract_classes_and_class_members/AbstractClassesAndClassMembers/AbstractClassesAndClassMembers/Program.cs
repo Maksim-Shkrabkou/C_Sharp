a32bbbba7fe3ea86d6ed3969983399145a981282
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-03 22:50:42 -0500

    Add Chapter_3_Classes_OOP part_8

---
+++ Chapter_3_Classes_OOP/Abstract_classes_and_class_members/AbstractClassesAndClassMembers/AbstractClassesAndClassMembers/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+
+namespace AbstractClassesAndClassMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            Client client = new Client ("Tom", 500);
+            Employee employee = new Employee ("Bob", "Apple");
+            client.Display ();
+            employee.Display ();
+            Console.WriteLine('\n');
+            
+            // 2!
+            Person client2 = new Client ("Tom", 500);
+            Person employee2 = new Employee ("Bob", "Operator");
+            Console.WriteLine('\n');
+            
+            // 3!
+            // Person person = new Person ("Bill"); // Error
+        }
+        
+        // Besides the usual classes, C # has abstract classes. An abstract class is like a regular class.
+        // It can also have variables, methods, constructors, properties.
+        // The only thing is that the abstract keyword is used when defining abstract classes:
+        
+        abstract class Human
+        {
+            public int Length { get; set; }
+            public double Weight { get; set; }
+        }
+        
+        // But the main difference is that we cannot use the constructor of an abstract class to create its object.
+        // For example, as follows:
+        
+        // Human h = new Human();
+        
+        // Why are abstract classes needed? For example, in our banking sector program, we can define two main entities:
+        // a bank customer and a bank employee. Each of these entities will be different, for example,
+        // for an employee, you need to define his position, and for a client - the amount on the account.
+        // Accordingly, Client and Employee will make up separate Client and Employee classes.
+        // At the same time, both of these entities can have something in common, for example,
+        // first and last name, some other common functionality. And it is better
+        // to move this general functionality into some separate class, for example, Person, which describes a person.
+        // That is, the Employee (employee) and Client (bank client) classes will derive from the Person class.
+        // And since all objects in our system will represent either a bank employee or a client,
+        // we will not create objects directly from the Person class. So it makes sense to make it abstract:
+        
+        abstract class Person
+        {
+            public string Name { get; set; }
+ 
+            public Person(string name)
+            {
+                Name = name;
+            }
+ 
+            public void Display()
+            {
+                Console.WriteLine(Name);
+            }
+        }
+ 
+        class Client : Person
+        {
+            public int Sum { get; set; }    // account amount
+ 
+            public Client(string name, int sum)
+                : base(name)
+            {
+                Sum = sum;
+            }
+        }
+ 
+        class Employee : Person
+        {
+            public string Position { get; set; } // position
+ 
+            public Employee(string name, string position) 
+                : base(name)
+            {
+                Position = position;
+            }
+        }
+        
+        // Then we can use these classes:
+        
+        // 1!
+
+        //Or even like this:
+
+        // 2!
+        
+        //But we can NOT create a Person object using the constructor of the Person class:
+        
+        // 3!
+        
+        // However, despite the fact that we cannot directly call
+        // the constructor of the Person class to create an object, nevertheless the constructor
+        // in abstract classes can play an important role, in particular,
+        // to initialize some variables and properties common to derived classes,
+        // as is the case with the property Name. Although the above example does not call the Person constructor,
+        // the Client and Employee derived classes can refer to it.
+        
+        
+        //
+        
+        
+        // !!!Abstract class members!!!
+        
+        // In addition to the usual properties and methods, an abstract class can have abstract class members,
+        // which are defined using the abstract keyword and have no functionality. In particular, abstract can be:
+
+        // Methods
+        // Properties
+        // Indexers
+        // Events
+        
+        // Abstract class members must not have the private modifier.
+        // In this case, the derived class must override and implement all abstract methods and properties
+        // that are in the base abstract class. When overridden in a derived class,
+        // such a method or property is also declared with the override modifier
+        // (as with normal overriding virtual methods and properties).
+        // It should also be noted that
+        // if a class has at least one abstract method (or abstract property, indexer, event),
+        // then this class must be defined as abstract.
+        
+        // Abstract members, like virtual ones, are part of the polymorphic interface.
+        // But if in the case of virtual methods we say that the inheriting class inherits the implementation,
+        // in the case of abstract methods, the interface represented by these abstract methods is inherited.
+
+        
+        //
+        
+        
+        // !!!Abstract methods!!!
+        
+        // For example, let's make the Display method abstract in the example above:
+        
+        abstract class Person2
+        {
+            public string Name { get; set; }
+ 
+            public Person2(string name)
+            {
+                Name = name;
+            }
+ 
+            public abstract void Display();
+        }
+ 
+        class Client2 : Person2
+        {
+            public int Sum { get; set; }    // account amount
+ 
+            public Client2(string name, int sum)
+                : base(name)
+            {
+                Sum = sum;
+            }
+            
+            public override void Display()
+            {
+                Console.WriteLine($"{Name} have account amount {Sum}");
+            }
+        }
+ 
+        class Employee2 : Person2
+        {
+            public string Position { get; set; } // position
+ 
+            public Employee2(string name, string position) 
+                : base(name)
+            {
+                Position = position;
+            }
+ 
+            public override void Display()
+            {
+                Console.WriteLine($"{Position} {Name}");
+            }
+        }
+        
+        
+        //
+        
+        
+        // !!!Abstract properties!!!
+        
+        // The use of abstract properties should be noted. Defining them is similar to defining auto properties.
+        // For example:
+        
+        abstract class Person3
+        {
+            public abstract string Name { get; set; }
+        }
+ 
+        class Client3 : Person3
+        {
+            private string name;
+ 
+            public override string Name
+            {
+                get => "Mr/Ms. " + name;
+                set => name = value;
+            }
+        }
+ 
+        class Employee3 : Person3
+        {
+            public override string Name { get; set; }
+        }
+
+        // The Person class defines the abstract Name property.
+        // It looks like an auto property, but it is not an auto property.
+        // Since this property does not have to be implemented, it only has empty get and set blocks.
+        // In derived classes, we can override this property to make it a full property (as in the Client class),
+        // or make it automatic (as in the Employee class).
+        
+        
+        //
+
+        
+        // !!!Refusal to implement abstract members!!!
+        
+        // The derived class must implement all the abstract members of the base class.
+        // However, we can drop the implementation, but in this case the derived class must also be defined as abstract:
+        
+        abstract class Person4
+        {
+            public abstract string Name { get; set; }
+        }
+ 
+        abstract class Manager4 : Person4
+        {
+        }
+        
+        
+        //
+        
+        // !!!Abstract class example!!!
+        
+        // The system of geometric figures is a textbook example. In reality, there is no geometric figure as such.
+        // There is a circle, a rectangle, a square, but there is simply no figure.
+        // However, both the circle and the rectangle have something in common and are shapes:
+        
+        // abstract shape class
+        abstract class Figure
+        {
+            // abstract method to get the perimeter
+            public abstract float Perimeter ();
+            // abstract method for getting area
+            public abstract float Area ();
+        }
+        
+        // derived class of the rectangle
+        class Rectangle: Figure
+        {
+            public float Width {get; set; }
+            public float Height {get; set; }
+ 
+            public Rectangle (float width, float height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+            // override getting the perimeter
+            public override float Perimeter ()
+            {
+                return Width * 2 + Height * 2;
+            }
+            // reassignment of getting area
+            public override float Area ()
+            {
+                return Width * Height;
+            }
+        }
+    }
+}
---
+++ Chapter_3_Classes_OOP/System.Object_class_and_methods/System.ObjectClassAndMethods/System.ObjectClassAndMethods/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+
+namespace System.ObjectClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            int i = 5;
+            Console.WriteLine (i.ToString ()); // will print the number 5
+ 
+            double d = 3.5;
+            Console.WriteLine (d.ToString ()); // will print the number 3.5
+            Console.WriteLine ('\n');
+            
+            // 2!
+            Person person = new Person { Name = "Tom" };
+            Console.WriteLine(person.ToString()); // will print the name of the Person class
+ 
+            Clock clock = new Clock { Hours = 15, Minutes = 34, Seconds = 53 };
+            Console.WriteLine(clock.ToString()); // will output 15:34:53
+            Console.WriteLine ('\n');
+            
+            // 3!
+            Person2 person2 = new Person2 { Name = "Tom" };
+            Console.WriteLine(person2);
+ 
+            Clock clock3 = new Clock { Hours = 15, Minutes = 34, Seconds = 53 };
+            Console.WriteLine(clock); // output 15:34:53
+            Console.WriteLine ('\n');
+            
+            // 4!
+            var person3 = new Person3("Tom");
+            Console.WriteLine(person3.GetHashCode());
+            Console.WriteLine ('\n');
+
+            // 5!
+            Person person4 = new Person { Name = "Tom" };
+            Console.WriteLine(person4.GetType()); // Person
+            Console.WriteLine ('\n');
+            
+            // 6!
+            object person5 = new Person {Name = "Tom"};
+            if (person5.GetType () == typeof (Person)) 
+                Console.WriteLine ("This is really a Person class");
+            Console.WriteLine(typeof (Person));
+            Console.WriteLine ('\n');
+            
+            // 7!
+            Person4 person6 = new Person4 { Name = "Tom" };
+            Person4 person7 = new Person4 { Name = "Bob" };
+            Person4 person8 = new Person4 { Name = "Tom" };
+            bool p1Ep2 = person6.Equals(person7);   // false
+            bool p1Ep3 = person6.Equals(person8);   // true
+            Console.WriteLine(p1Ep2);
+            Console.WriteLine(p1Ep3);
+        }
+        
+        // All other classes in .NET, even those we create ourselves, as well as base types such as System.Int32,
+        // are implicitly derived from the Object class. Even if we do not specify the Object class as the base class,
+        // by default, the Object class is implicitly at the top of the inheritance hierarchy. Therefore, all types
+        // and classes can implement the methods that are defined in the System.Object class. Let's consider these methods.
+
+        // !!!ToString!!!
+        
+        // The ToString method is used to get the string representation of this object.
+        // For basic types, their string value will simply be displayed:
+        
+        // 1!
+        
+        // For classes, this method displays the full name of the class with an indication
+        // of the namespace in which this class is defined. And we can override this method. Let's see an example:
+        
+        class Clock
+        {
+            public int Hours { get; set; }
+            public int Minutes { get; set; }
+            public int Seconds { get; set; }
+            
+            public override string ToString()
+            {
+                return $"{Hours}:{Minutes}:{Seconds}";
+            }
+        }
+        
+        class Person
+        {
+            public string Name { get; set; }
+        }
+        
+        // 2!
+        
+        
+        // The override keyword is used to override the ToString method in the Clock class,
+        // which represents a clock (as with the usual overriding of virtual or abstract methods).
+        // In this case, the ToString () method is the value of the Hours, Minutes, Seconds properties, combined into one string.
+
+        // The Person class does not override the ToString method,
+        // so the standard implementation of this method works for this class, which simply displays the name of the class.
+
+        // By the way, in this case, we could use both implementations:
+        
+        class Person2
+        {
+            public string Name { get; set; }
+            
+            public override string ToString()
+            {
+                if (String.IsNullOrEmpty(Name))
+                    return base.ToString();
+                
+                return Name;
+            }
+        }
+        
+        // That is, if the name - the Name property has no value, it represents an empty string,
+        // then the base implementation is returned - the name of the class.
+        // If the name is set, then the value of the Name property is returned.
+        // The String.IsNullOrEmpty () method is used to test a string for emptiness.
+
+        // It is worth noting that various technologies on the .NET platform actively
+        // use the ToString method for different purposes. In particular, the same Console.WriteLine () method
+        // by default outputs exactly the string representation of the object.
+        // Therefore, if we need to display the string representation of an object to the console,
+        // then when passing the object to the Console.WriteLine method,
+        // it is not necessary to use the ToString () method - it is called implicitly:
+
+        
+        // 3!
+        
+        
+        //
+        
+        
+        // !!!GetHashCode method!!!
+        
+        // The GetHashCode method allows you to return some numeric value that will correspond to the given object
+        // or its hash code. By this number, for example, you can compare objects.
+        // You can define a variety of algorithms for generating such a number, or take an implementation of the base type:
+        
+        class Person3
+        {
+            public string Name { get; set; }
+
+            public Person3(string name)
+            {
+                Name = name;
+            }
+
+            public override int GetHashCode()
+            {
+                return Name.GetHashCode();
+            }
+        }
+        
+        
+        // 4!
+        
+        // In this case, the GetHashCode method returns a hash code for the value of the Name property
+        // . That is, two Person objects that have the same name will return the same hash code.
+        // However, in reality, the algorithm can be very different.
+        
+        
+        //
+        
+        
+        // Getting the object type and the GetType method
+        
+        // The GetType method allows you to get the type of this object:
+        
+        // 5!
+        
+        //This method returns a Type object, that is, the type of the object.
+
+        //With the typeof keyword we get the type of the class and compare it with the type of the object.
+        //And if this object represents the Client type, then we perform certain actions.
+        
+        // 6!
+        
+        // Moreover, since the Object class is the base type for all classes,
+        // we can assign an object of any type to a variable of the object type.
+        // However, for this variable, the GetType method will still return the type that the variable refers to.
+        // That is, in this case, an object of type Person.
+
+        // Unlike the ToString, Equals, GetHashCode methods, the GetType method is not overridden.
+        
+        
+        //
+        
+        
+        
+        // !!!Equals method!!!
+        
+        // The Equals method allows you to compare two objects for equality:
+        
+        class Person4
+        {
+            public string Name { get; set; }
+            
+            public override bool Equals(object obj)
+            {
+                if (obj.GetType() != this.GetType()) return false;
+                
+                Person4 person = (Person4)obj;
+                
+                return (this.Name == person.Name);
+            }
+        }
+        
+        // The Equals method takes an object of any type as a parameter,
+        // which we then cast to the current one if they are objects of the same class.
+        // Then we compare by name. If the names are equal, we return true, which will say that the objects are equal.
+        // However, if necessary, the implementation of the method can be made more complex,
+        // for example, it can be compared by several properties, if any.
+
+        // Application of the method:
+        
+        // 7!
+        
+        // And if you need to compare two complex objects, as in this case,
+        // then it is better to use the Equals method rather than the standard == operation.
+    }
+}
