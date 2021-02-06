9d5f90c79018cbf5de407fb5bbee5c5b81e0ed8c
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-06 17:00:54 -0500

    Add Chapter_4_Exception_handling part_1

---
+++ Chapter_4_Exception_handling/Creating_exception_classes/CreatingExceptionClasses/CreatingExceptionClasses/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace CreatingExceptionClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            try
+            {
+                var p = new Person { Name = "Tom", Age = 17 };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            
+            Console.WriteLine("\n");
+            
+            // 2!
+            try
+            {
+                var p = new Person2 {Name = "Tom", Age = 13};
+            }
+            catch (PersonException ex)
+            {
+                Console.WriteLine ($"Error: {ex.Message}");
+                Console.WriteLine ($"Invalid value: {ex.Value}");
+            }
+        }
+        
+        // If we are not satisfied with the built-in exception types, then we can create our own types.
+        // The base class for all exceptions is the Exception class, so we can inherit this class to create our own types.
+
+        // Let's say we have an age limit in our program:
+        
+        // 1!
+        
+        class Person
+        {
+            private int age;
+            public string Name { get; set; }
+            
+            public int Age
+            {
+                get { return age; }
+                set
+                {
+                    if (value < 18)
+                    {
+                        throw new Exception("Registration is prohibited for persons under 18");
+                    }
+                    else
+                    {
+                        age = value;
+                    }
+                }
+            }
+        }
+        
+        // In the Person class, when the age is set, a check occurs, and if the age is less than 18, an exception is thrown.
+        // The Exception class takes a string as a parameter in its constructor, which is then passed to its Message property.
+
+        // But sometimes it is more convenient to use your own exception classes.
+        // For example, in some situation,
+        // we want to handle in a certain way only those exceptions that belong to the Person class.
+        // For these purposes, we can make a special PersonException class:
+
+        /*class PersonException : Exception
+        {
+            public PersonException(string? message)
+                : base(message)
+            {
+            }
+        }*/
+        
+        // In fact, the class has nothing except an empty constructor, and then in the constructor
+        // we simply refer to the constructor of the base Exception class, passing the message string to it.
+        // But now we can change the Person class so that it throws an exception of this type and, accordingly,
+        // handle this exception in the main program:
+
+        // Each type of exception can define some of its properties. For example, in this case,
+        // we can define a property in the class to store the set value:
+
+       
+        class PersonException: ArgumentException
+        {
+            public int Value {get;}
+            
+            public PersonException (string message, int val)
+                : base (message)
+            {
+                Value = val;
+            }
+        }
+        
+        // In the constructor of the class, we set this property and when handling the exception, we can get it:
+        
+        // 2!
+        
+        class Person2
+        {
+            public string Name { get; set; }
+            private int _age;
+            
+            public int Age
+            {
+                get => _age;
+                set
+                {
+                    if (value < 18)
+                        throw new PersonException("Registration is prohibited for persons under 18", value);
+                    else
+                        _age = value;
+                }
+            }
+        }
+    }
+}
---
+++ Chapter_4_Exception_handling/Exception_types_and_exception_class/ExceptionTypesExceptionClass/ExceptionTypesExceptionClass/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace ExceptionTypesExceptionClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            try
+            {
+                var x = 5;
+                var y = x / 0;
+                Console.WriteLine ($"Result: {y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine ($"Exception: {ex.Message}");
+                Console.WriteLine ($"Method: {ex.TargetSite}");
+                Console.WriteLine ($"Stack trace: {ex.StackTrace}");
+                Console.WriteLine($"Inner exception: {ex.InnerException}");
+                Console.WriteLine($"Source: {ex.Source}");
+                Console.WriteLine("\n");
+            }
+            
+            // 2!
+            try
+            {
+                var numbers = new int[4];
+                numbers[7] = 9;     // IndexOutOfRangeException
+ 
+                var x = 5;
+                var y = x / 0;  // DivideByZeroException
+                Console.WriteLine($"Result: {y}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("An exception was thrown -> DivideByZeroException");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.WriteLine("\n");
+            
+            // 3!
+            try
+            {
+                object obj = "you";
+                var num = (int) obj; // InvalidCastException
+                Console.WriteLine ($"Result: {num}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine ("DivideByZeroException thrown");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine ("An IndexOutOfRangeException was thrown");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }  
+            
+            Console.WriteLine("\n");
+        }
+        
+        // The type of exception is basic for all types of exceptions.
+        // This type defines a number of properties that can be used to obtain information about an exception.
+
+        // InnerException: Stores information about the exception.
+        
+        // Message: stores the exception message, error text
+
+        // Source: Stores the name of the object or assembly that threw the exception
+
+        // StackTrace: returns a string representation of the call stack that throws the exception
+
+        // TargetSite: Returns the method where the exception was thrown
+
+        // For example, let's handle an exception of type Exception:
+        
+        // 1!
+        
+        
+        // However, since the Exception type is the base type for all exceptions,
+        // the catch (Exception ex) expression will handle any exceptions that might be thrown.
+
+        // But there are also more specialized types of exceptions that are designed to handle specific types of exceptions.
+        // There are quite a few of them, I will give just a few:
+
+        // DivideByZeroException: Represents the exception that is thrown when divided by zero
+
+        // ArgumentOutOfRangeException: Thrown if the argument value is out of range
+
+        // ArgumentException: thrown if an invalid value is passed to the method for a parameter
+
+        // IndexOutOfRangeException: Thrown if the index of an element in an array or collection is out of range
+        
+        // InvalidCastException: Thrown when trying to perform invalid type conversions
+
+        // NullReferenceException: thrown when trying to access an object that is null (that is, inherently undefined)
+
+        // And if necessary, we can differentiate the handling of different types of exceptions by including additional catch blocks:
+        
+        // 2!
+        
+        // In this case, the catch blocks handle exceptions of the IndexOutOfRangeException, DivideByZeroException, and Exception types. When an exception is thrown in the try block, the CLR will look for the correct catch block to handle the exception. So, in this case, on the line
+            
+        // numbers [7] = 9;
+        
+        // the 7th element of the array is accessed. However, since there are only 4 elements in the array,
+        // we will get an IndexOutOfRangeException.
+        // The CLR will find a catch block that handles the exception and transfer control to it.
+        
+        // It should be noted that in this case in the try block there is a situation
+        // for throwing a second exception - division by zero.
+        // However, since after the IndexOutOfRangeException is thrown, control passes to the corresponding catch block,
+        // division by zero int y = x / 0 will in principle not be performed,
+        // so an exception of the DivideByZeroException type will never be thrown.
+
+        // However, consider a different situation:
+        
+        /*
+        try
+        {
+            object obj = "you";
+            var num = (int) obj; // InvalidCastException
+            Console.WriteLine ($"Result: {num}");
+        }
+        catch (DivideByZeroException)
+        {
+        Console.WriteLine ("DivideByZeroException thrown");
+        }
+        catch (IndexOutOfRangeException)
+        {
+        Console.WriteLine ("An IndexOutOfRangeException was thrown");
+        }
+        */
+        
+        // In this case, an InvalidCastException exception is thrown in the try block,
+        // but there is no corresponding catch block to handle this exception. Therefore, the program will crash.
+
+        // We can also define our own catch block for InvalidCastException,
+        // but the point is that, in theory, various types of exceptions themselves can be thrown in the code.
+        // And it makes no sense to define catch blocks for all types of exceptions if the exception handling is the same.
+        // And in this case, we can define a catch block for the base Exception type:
+        
+        // 3!
+        
+        // And in this case, the catch (Exception ex) {} block will handle all exceptions
+        // except DivideByZeroException and IndexOutOfRangeException. In this case, catch blocks for more general,
+        // more basic exceptions should be placed at the end - after catch blocks for more specific, specialized types.
+        // Because the CLR chooses to handle the exception with the first catch block that matches the type of exception thrown.
+        // Therefore, in this case,
+        // the DivideByZeroException and IndexOutOfRangeException exception is first handled, and only then the Exception
+        // (since DivideByZeroException and IndexOutOfRangeException are inherited from the Exception class).
+    }
+}
