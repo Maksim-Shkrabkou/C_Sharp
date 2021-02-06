7a64aa6f8acf5b52c26a6f67621b5dd1a2131c91
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-02-06 17:39:29 -0500

    Add Chapter_4_Exception_handling final part

---
+++ Chapter_4_Exception_handling/Finding_catch_block_when_handling_exceptions/FindingCatchBlockWhenHandlingExceptions/FindingCatchBlockWhenHandlingExceptions/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace FindingCatchBlockWhenHandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch in Main: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine ("finally block in Main");
+            }
+            
+            Console.WriteLine ("End of Main method");
+        }
+        
+        
+        // If the code that raises the exception is not placed in the try block or is placed in the try..catch construction
+        // that does not contain the corresponding catch block to handle the thrown exception,
+        // then the system searches for the corresponding exception handler in the call stack.
+
+        // For example, consider the following program:
+        
+        // 1!
+        
+        class TestClass
+        {
+            public static void Method1()
+            {
+                try
+                {
+                    Method2();
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Catch in Method1 : {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Block finally in Method1");
+                }
+                
+                Console.WriteLine("End of Method1");
+            }
+            
+            static void Method2()
+            {
+                try
+                {
+                    var x = 8;
+                    var y = x / 0;
+                }
+                finally
+                {
+                    Console.WriteLine("Block finally в Method2");
+                }
+                
+                Console.WriteLine("End of Method2");
+            }
+        }
+        
+        // In this case, the call stack looks like this: the Main method calls Method1, which in turn calls Method2.
+        // And in Method2, a DivideByZeroException is thrown. Visually, the call stack can be represented as follows:
+
+        // Finding a catch block while handling an exception in C #
+        // At the bottom of the stack is the Main method that started execution, and at the very top is Method2.
+
+        // What will happen in this case when an exception is thrown?
+
+        // 1. The Main method calls Method1, which calls Method2, which throws a DivideByZeroException.
+
+        // 2. The system sees that the code that caused the exception was placed in the try..catch construction
+        
+        /*try
+        {
+            int x = 8;
+            int y = x / 0;
+        }
+        finally
+        { 
+        Console.WriteLine ("finally block in Method2");
+        }*/
+    
+        // The system looks for a catch block in this construction that handles the DivideByZeroException exception.
+        // However, there is no such catch block.
+        
+        
+        // 3. The system is dropped in the call stack into the Method1 method that called Method2.
+        // Here Method2 call is placed in try..catch construct
+        
+        /*try
+        {
+            Method2 ();
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine ($ "Catch in Method1: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine ("finally block in Method1");
+        }*/
+    
+        // The system also looks in this construction for a catch block that handles the DivideByZeroException exception.
+        // However, there is no such catch block here either.
+        
+        // 4. The system then descends on the call stack to the Main method that called Method1.
+        // Here the call to Method1 is placed in the try..catch construct
+            
+        /*
+        try
+        {
+            TestClass.Method1 ();
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine ($ "Catch in Main: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine ("finally block in Main");
+        }*/
+        
+        // The system again looks for a catch block in this construction that handles the DivideByZeroException exception.
+        // And in this case, a weaving block was found.
+        
+        // 5. The system finally found the right catch block in the Main method to handle the exception
+        // that was thrown in the Method2 - that is, to the initial method where the exception was thrown directly.
+        // But so far this catch block is NOT executed.
+        // The system climbs back up the call stack to the top of Method2 and executes a finally block there:
+        
+        /*finally
+        {
+            Console.WriteLine ("finally block in Method2");
+        }*/
+        
+        // 6. Then the system goes back down the call stack to Method1 and executes the finally block in it:
+        
+        /*
+        finally
+        {
+            Console.WriteLine ("finally block in Method1");
+        }*/
+        
+        // 7. The system then steps down the call stack to the Main method and executes the catch block it finds and the subsequent finally block there:
+        
+        /*catch (DivideByZeroException ex)
+        {
+            Console.WriteLine ($ "Catch in Main: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine ("finally block in Main");
+        }*/
+        
+        // 8. Next, the code that goes in the Main method after the try..catch construction is executed:
+        
+        /*
+         * Console.WriteLine ("End of Main method");
+         */
+            
+        // It is worth noting that the code that follows the try ... catch construct in the Method1
+        // and Method2 methods is not executed, because the exception handler was found in the Main method.
+    }
+}
---
+++ Chapter_4_Exception_handling/Throwing_an_exception_and_the_throw_statement/ThrowingAnExceptionAndTheThrowStatement/ThrowingAnExceptionAndTheThrowStatement/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ThrowingAnExceptionAndTheThrowStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1!
+            try
+            {
+                Console.Write ("Enter string:");
+                var message = Console.ReadLine ();
+                
+                if (message != null && message.Length > 6)
+                {
+                    throw new Exception("String length is more than 6 characters");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine ($"Error: {e.Message}");
+            }
+            
+            // 2!
+            try
+            {
+                try
+                {
+                    Console.Write ("Enter string:");
+                    var message = Console.ReadLine ();
+                    
+                    if (message.Length> 6)
+                    {
+                        throw new Exception ("String length is more than 6 characters");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine ("An exception was thrown");
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine (ex.Message);
+            }
+        }
+        
+        // Usually, the system itself throws exceptions in certain situations, for example, when dividing a number by zero.
+        // But C # also allows you to manually throw exceptions using the throw statement.
+        // That is, with the help of this operator, we ourselves can create an exception and raise it at runtime.
+
+        // For example, in our program, a string is being input,
+        // and we want an exception to be thrown if the length of the string is more than 6 characters:
+        
+        // 1!
+        
+        // After the throw operator, an exception object is specified,
+        // through the constructor of which we can transmit an error message.
+        // Naturally, instead of the Exception type, we can use an object of any other type of exceptions.
+
+        // Then, in the catch block, the exception we thrown will be handled.
+
+        // Similarly, we can throw exceptions anywhere in the program.
+        // But there is also another form of using the throw statement, where no exception object is specified after the statement.
+        // In this form, the throw statement can only be used in a catch block:
+        
+        // 2!
+        
+        // In this case, when you enter a string with a length of more than 6 characters, an exception will be thrown,
+        // which will be handled by the inner catch block. However, since this block uses a throw statement,
+        // the exception will be passed on to the outer catch block.
+    }
+}
